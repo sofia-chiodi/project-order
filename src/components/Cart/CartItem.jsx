30862ed7700205191ff80d3102bc89177336ad10
sofia-chiodi <chiodi.sb@gmail.com>
2024-10-31 17:43:39 -0300

    CartItem created

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,1 +1,22 @@
-export default function CartItem() {}
+import { currencyFormatter } from '../../util/formatting';
+
+export default function CartItem({
+  name,
+  quantity,
+  price,
+  onIncrease,
+  onDecrease,
+}) {
+  return (
+    <li>
+      <p>
+        {name} - {quantity} * {currencyFormatter.format(price)}
+      </p>
+      <p>
+        <button onClick={onDecrease}>-</button>
+        <span>{quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </p>
+    </li>
+  );
+}
