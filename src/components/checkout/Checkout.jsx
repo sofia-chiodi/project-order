1633e0de57a44d44c5a6ee5b8b8ba2ef4cc873da
sofia-chiodi <chiodi.sb@gmail.com>
2024-11-03 19:48:18 -0300

    Adding onClose method to Cart

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -20,13 +20,33 @@ export default function Checkout() {
     userProgressCtx.hideCheckout();
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries()); // {email: test@123.com}
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
+
   return (
-    <Modal open={userProgressCtx.progress === 'checkout'}>
-      <form>
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
-        <Input label='Full name' type='text' id='full-name' />
+        <Input label='Full name' type='text' id='name' />
         <Input label='E-mail address' type='email' id='email' />
         <Input label='Street' type='text' id='street' />
         <div className='control-row'>
